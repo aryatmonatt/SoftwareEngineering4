7d8b0b04ab77cd176d38394f8f7009ec3956a92e
Arya <Arya@192.168.1.103>
2023-10-21 17:36:59 +1100

    "testing"

--- Asignment_4/src/part1/Testing.java
+++ Asignment_4/src/part1/Testing.java
@@ -4,7 +4,7 @@ public class Testing {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("hello my name is");
+		System.out.print("hello my name is arya");
 	}
 
 }
